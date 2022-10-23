ab5ff57248f378605c4d2b28129fd6bc8d965984
vega <devid367552@gmail.com>
2022-10-23 13:20:01 +0300

    without tests 0.1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,54 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int a = inputValuesInArray();
+        System.out.println(a);
     }
+
+
+
+    public static int inputValuesInArray(){
+        Scanner scn = new Scanner (System.in);
+        System.out.println("Input size of array");
+        final int SIZE = scn.nextInt();
+        int[] arr = new int[SIZE];
+        System.out.println("input values of array");
+        for(int i = 0; i < SIZE; i++){
+            arr[i] = scn.nextInt();
+        }
+        return solution(arr);
+    }
+
+    static int solution(int[] a){
+        int counter = 0;
+        int countermax = 0;
+        int countermin = 0;
+        for(int i = 1; i < a.length - 1; i++){
+            if (a[i] > a[i-1] && a[i] > a[i+1]){
+                countermax++;
+            }
+
+            if (a[i] < a[i-1] && a[i] < a[i+1]){
+                countermin++;
+            }
+        }
+        if(a[0] > a[1]){
+            countermax++;
+        }
+        if(a[a.length - 1] > a[a.length - 2 ]){
+            countermax++;
+        }
+        if(a[0] < a[1]){
+            countermin++;
+        }
+        if(a[a.length - 1] < a[a.length - 2 ]){
+            countermin++;
+        }
+        System.out.println(Arrays.toString(a));
+        return countermin + countermax;
+    }
+
+
 }
